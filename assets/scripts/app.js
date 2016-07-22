aa12135f6e3e016244230ae4a97cd5d00347c1a8
AustinMahan <amahan0424@gmail.com>
2016-07-22 15:12:16 -0600

    fixed ajax call

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,39 +1,35 @@
 // Your code here!
 $(document).ready(function() {
   $.ajax({
-    url:'http://galvanize-student-apis.herokuapp.com/gpersonnel/roles',
+    url:'https://galvanize-student-apis.herokuapp.com/gpersonnel/roles',
     method: "GET"
   }).done(function (data){
-    // console.log(data);
-    for(var i = 0; i < data.length; i++){
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
       $('#roleSelect').append('<option value="' + data[i].title + '">' + data[i].title + '</option>')
     }
     $('select').change(function(){
       data.forEach(function(role){
-        if(role.title == $('#roleSelect').val()){
-          $('img').attr('src', role.img)
-        }
-      })
-    })
-  })
+        if (role.title == $('#roleSelect').val()) {
+          $('img').attr('src', role.img);
+        };
+      });
+    });
+  });
 
 
 
 
   $('button').click(function() {
-    if($('#fName').val().length !== 0 && $('#lName').val().length !== 0 ){
+    if ($('#fName').val().length !== 0 && $('#lName').val().length !== 0 ) {
       var fName = $('#fName').val()
       var lName = $('#lName').val()
       var role = $('#roleSelect').val()
-      console.log(fName);
-      console.log(lName);
-      console.log(role);
       $.ajax({
         url:'http://galvanize-student-apis.herokuapp.com/gpersonnel/users',
         method: 'POST',
         data: { firstName: fName, lastName: lName, role: role },
         success: function(suc){
-          console.log(suc);
           $('.success').append(suc.message);
           $('.success').fadeIn()
           setTimeout(function(){
